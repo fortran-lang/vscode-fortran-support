865d479673bfee303aa45baf074dae67a80f91dc
Miguel Carvajal <krvajal.miguelangel@gmail.com>
2017-05-29 10:40:31 -0300

    Add hover provider for docs

--- src/extension.ts
+++ src/extension.ts
@@ -2,9 +2,13 @@
 import * as vscode from 'vscode'; 
 
 import FortranLintingProvider from './features/linter-provider';
+import FortranHoverProvider from "./features/hover-provider";
 
 export function activate(context: vscode.ExtensionContext) {
 	let linter = new FortranLintingProvider();	
+	let hover = new FortranHoverProvider();	
 	linter.activate(context.subscriptions);
 	vscode.languages.registerCodeActionsProvider('fortran90', linter);
+	vscode.languages.registerHoverProvider('fortran90', hover);
+	
 }
