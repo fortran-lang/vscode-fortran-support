3023d2fae07994e3642cb44857c97e30f4d7b57f
Miguel Carvajal <krvajal.miguelangel@gmail.com>
2017-07-04 20:05:20 -0300

    activate completion provider

--- src/extension.ts
+++ src/extension.ts
@@ -3,18 +3,20 @@ import * as vscode from 'vscode';
 
 import FortranLintingProvider from './features/linter-provider';
 import FortranHoverProvider from "./features/hover-provider";
+import { FortranCompletionProvider } from "./features/completion-provider";
 
 export function activate(context: vscode.ExtensionContext) {
 	
-	let hover = new FortranHoverProvider();	
-	
+	let hoverProvider = new FortranHoverProvider();	
+	let completionProvider = new FortranCompletionProvider();
 	if(vscode.workspace.getConfiguration("fortran").get("linterEnabled", true)){
 		
 		let linter = new FortranLintingProvider();	
 		linter.activate(context.subscriptions);
 		vscode.languages.registerCodeActionsProvider('fortran90', linter);
 	}
-
-	vscode.languages.registerHoverProvider('fortran90', hover);
+	
+	vscode.languages.registerCompletionItemProvider("fortran90",completionProvider)
+	vscode.languages.registerHoverProvider('fortran90', hoverProvider);
 	
 }
