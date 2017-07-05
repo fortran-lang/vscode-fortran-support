0b4c7a660b52534b0dc43e66f9374fa7c124cfe3
notZaki <zaki@live.ca>
2017-07-05 16:20:53 -0400

    Make linters errorRegex work on windows

--- src/features/linter-provider.ts
+++ src/features/linter-provider.ts
@@ -16,7 +16,7 @@ export default class FortranLintingProvider {
 
     private diagnosticCollection: vscode.DiagnosticCollection;
     private doModernFortranLint(textDocument: vscode.TextDocument) {
-        const errorRegex: RegExp = /^([^:]*):([0-9]+):([0-9]+):\n\s(.*)\n.*\n(Error|Warning|Fatal Error):\s(.*)$/gm;
+        const errorRegex: RegExp = /^.{2}([^:]*):([0-9]+):([0-9]+):\r?\n\s*(.*)\r?\n.*\r?\n(Error|Warning|Fatal Error):\s(.*)$/gm;;
 
         if (textDocument.languageId !== LANGUAGE_ID) {
             return;
