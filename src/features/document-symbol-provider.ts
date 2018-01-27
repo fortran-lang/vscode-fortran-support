9e9eb709088b209a0cf1338f7cbe3ced483962c7
Miguel Carvajal <krvajal.miguelangel@gmail.com>
2018-01-27 18:50:36 -0300

    Make document symbol provider async

--- src/features/document-symbol-provider.ts
+++ src/features/document-symbol-provider.ts
@@ -1,31 +1,64 @@
-
-
 import { CancellationToken, TextDocument, Position, Hover } from "vscode";
 
-import * as vscode from 'vscode';
-import { getDeclaredFunctions, getDeclaredSubroutines} from '../lib/functions';
-import { getDeclaredVars } from '../lib/variables';
+import * as vscode from "vscode";
+import { getDeclaredFunctions, getDeclaredSubroutines } from "../lib/functions";
+import { getDeclaredVars } from "../lib/variables";
 
-export class FortranDocumentSymbolProvider implements vscode.DocumentSymbolProvider {
+export class FortranDocumentSymbolProvider
+  implements vscode.DocumentSymbolProvider {
+  vars: Array<vscode.SymbolInformation>;
+  functions: Array<vscode.SymbolInformation>;
+  subroutines: Array<vscode.SymbolInformation>;
 
-    public provideDocumentSymbols(document: TextDocument, token: CancellationToken){
+  public provideDocumentSymbols(
+    document: TextDocument,
+    token: CancellationToken
+  ): Thenable<vscode.SymbolInformation[]> {
+    return new Promise<vscode.SymbolInformation[]>((resolve, reject) => {
+      token.onCancellationRequested(e => {
+        reject();
+      });
+      this.updateFunctionDefinitions(document);
+      this.updateSubroutineDefinitions(document);
+      this.updateVariablesDefiniton(document);
+      resolve([...this.functions, ...this.subroutines, ...this.vars]);
+    });
+  }
 
-        let functions = getDeclaredFunctions(document).map(fun => {
+  private updateFunctionDefinitions(document: TextDocument) {
+    this.functions = getDeclaredFunctions(document).map(fun => {
+      let range = new vscode.Range(fun.lineNumber, 0, fun.lineNumber, 100);
+      return new vscode.SymbolInformation(
+        fun.name,
+        vscode.SymbolKind.Function,
+        range
+      );
+    });
+  }
 
-            let range = new vscode.Range(fun.lineNumber, 0, fun.lineNumber, 100);
-            return new vscode.SymbolInformation(fun.name, vscode.SymbolKind.Function, range );
-        });
-        let subroutines = getDeclaredSubroutines(document).map(fun => {
-
-            let range = new vscode.Range(fun.lineNumber, 0, fun.lineNumber, 100);
-            return new vscode.SymbolInformation(fun.name, vscode.SymbolKind.Function, range );
-        });
-        let vars = getDeclaredVars(document).map(variable => {
-            let range = new vscode.Range(variable.lineNumber, 0, variable.lineNumber, 100);
-            return new vscode.SymbolInformation(variable.name, vscode.SymbolKind.Variable, range );
-        });
-
-        return [...functions, ...subroutines, ...vars];
-    }
-
+  private updateSubroutineDefinitions(document: TextDocument) {
+    this.subroutines = getDeclaredSubroutines(document).map(fun => {
+      let range = new vscode.Range(fun.lineNumber, 0, fun.lineNumber, 100);
+      return new vscode.SymbolInformation(
+        fun.name,
+        vscode.SymbolKind.Function,
+        range
+      );
+    });
+  }
+  private updateVariablesDefiniton(document: TextDocument) {
+    this.vars = getDeclaredVars(document).map(variable => {
+      let range = new vscode.Range(
+        variable.lineNumber,
+        0,
+        variable.lineNumber,
+        100
+      );
+      return new vscode.SymbolInformation(
+        variable.name,
+        vscode.SymbolKind.Variable,
+        range
+      );
+    });
+  }
 }
--- src/lib/functions.ts
+++ src/lib/functions.ts
@@ -1,112 +1,102 @@
-
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 export interface Variable {
-    name: string;
-    type?: string;
-    lineNumber?: number;
+  name: string;
+  type?: string;
+  lineNumber?: number;
 }
-
 
 export interface Subroutine {
-
-    name: string;
-    args: Variable[];
-    docstr: string;
-    lineNumber: number
+  name: string;
+  args: Variable[];
+  docstr: string;
+  lineNumber: number;
 }
 
-export interface Function extends Subroutine {    
-
-    return: Variable; // function is a subroutine with return type
+export interface Function extends Subroutine {
+  return: Variable; // function is a subroutine with return type
 }
 
 export enum MethodType {
-    Subroutine,
-    Function
-};
-
-
-
-export function getDeclaredFunctions(document: vscode.TextDocument): Function[] {
-
-    let lines = document.lineCount;
-    let funcs = [];
-
-    for (let i = 0; i < lines; i++) {
-        let line: vscode.TextLine = document.lineAt(i);
-        if (line.isEmptyOrWhitespace) continue;
-        let newFunc = parseFunction(line.text)
-        if(newFunc){
-            funcs.push({...newFunc, lineNumber: i });
-        }
-    }
-    return funcs;
+  Subroutine,
+  Function
 }
 
-export function getDeclaredSubroutines(document: vscode.TextDocument):Subroutine[]{
+export function getDeclaredFunctions(
+  document: vscode.TextDocument
+): Function[] {
+  let lines = document.lineCount;
+  let funcs = [];
 
-    let lines = document.lineCount;
-    let subroutines = [];
-
-    for (let i = 0; i < lines; i++) {
-        let line: vscode.TextLine = document.lineAt(i);
-        if (line.isEmptyOrWhitespace) continue;
-        let newSubroutine = parseSubroutine(line.text)
-        if(newSubroutine){
-            subroutines.push({...newSubroutine, lineNumber: i });
-        }
+  for (let i = 0; i < lines; i++) {
+    let line: vscode.TextLine = document.lineAt(i);
+    if (line.isEmptyOrWhitespace) continue;
+    let newFunc = parseFunction(line.text);
+    if (newFunc) {
+      funcs.push({ ...newFunc, lineNumber: i });
     }
-    return subroutines;
+  }
+  return funcs;
 }
 
+export function getDeclaredSubroutines(
+  document: vscode.TextDocument
+): Subroutine[] {
+  let lines = document.lineCount;
+  let subroutines = [];
 
+  for (let i = 0; i < lines; i++) {
+    let line: vscode.TextLine = document.lineAt(i);
+    if (line.isEmptyOrWhitespace) continue;
+    let newSubroutine = parseSubroutine(line.text);
+    if (newSubroutine) {
+      subroutines.push({ ...newSubroutine, lineNumber: i });
+    }
+  }
+  return subroutines;
+}
 
 export const parseFunction = (line: string) => {
-
-    return _parse(line, MethodType.Function);
-}
+  return _parse(line, MethodType.Function);
+};
 
 export const parseSubroutine = (line: string) => {
-
-    return _parse(line, MethodType.Subroutine);
-}
+  return _parse(line, MethodType.Subroutine);
+};
 export const _parse = (line: string, type: MethodType) => {
-
-    const functionRegEx = /([a-zA-Z]+(\([\w.=]+\))*)*\s*function\s*([a-zA-Z_][a-zA-Z0-9_]*)\s*\((\s*[a-zA-Z_][a-zA-Z0-9_,\s]*)*\s*\)\s*(result\([a-zA-Z_][\w]*\))*/g;
-    const subroutineRegEx = /subroutine\s*([a-zA-Z][a-zA-Z0-9_]*)\s*\((\s*[a-zA-Z][a-zA-z0-9_,\s]*)*\s*\)/g;
-    const regEx = (type === MethodType.Subroutine) ? subroutineRegEx : functionRegEx;
-    if (line.match(regEx) && type === MethodType.Function) {
-        let [attr, kind_descriptor, name, argsstr, result] = regEx.exec(line).slice(1, 5);
-        let args = (argsstr) ? parseArgs(argsstr) : [];
-        return {
-            name: name,
-            args: args
-        };
-    } else if (line.match(regEx) && type === MethodType.Subroutine) {
-        let [name, argsstr] = regEx.exec(line).slice(1);
-        let args = (argsstr) ? parseArgs(argsstr) : [];
-        return {
-            name: name,
-            args: args
-        };
-    } 
-
-}
-
-
-
+  const functionRegEx = /([a-zA-Z]+(\([\w.=]+\))*)*\s*function\s*([a-zA-Z_][a-zA-Z0-9_]*)\s*\((\s*[a-zA-Z_][a-zA-Z0-9_,\s]*)*\s*\)\s*(result\([a-zA-Z_][\w]*\))*/gi;
+  const subroutineRegEx = /subroutine\s*([a-zA-Z][a-zA-Z0-9_]*)\s*\((\s*[a-zA-Z][a-zA-z0-9_,\s]*)*\s*\)/gi;
+  const regEx =
+    type === MethodType.Subroutine ? subroutineRegEx : functionRegEx;
+  if (line.match(regEx) && type === MethodType.Function) {
+    let [attr, kind_descriptor, name, argsstr, result] = regEx
+      .exec(line)
+      .slice(1, 5);
+    let args = argsstr ? parseArgs(argsstr) : [];
+    return {
+      name: name,
+      args: args
+    };
+  } else if (line.match(regEx) && type === MethodType.Subroutine) {
+    let [name, argsstr] = regEx.exec(line).slice(1);
+    let args = argsstr ? parseArgs(argsstr) : [];
+    return {
+      name: name,
+      args: args
+    };
+  }
+};
 
 export const parseArgs = (argsstr: string) => {
-    let args = argsstr.trim().split(',');
-    let variables: Variable[] = args.filter(name => validVariableName(name))
-        .map(name => {
-            return { name: name };
-        });
-    return variables;
-}
+  let args = argsstr.trim().split(",");
+  let variables: Variable[] = args
+    .filter(name => validVariableName(name))
+    .map(name => {
+      return { name: name };
+    });
+  return variables;
+};
 
 export const validVariableName = (name: string) => {
-    return name.trim().match(/^[a-zA-Z_][a-zA-Z0-9_]*$/) !== null;
-}
-
+  return name.trim().match(/^[a-zA-Z_][a-zA-Z0-9_]*$/) !== null;
+};
