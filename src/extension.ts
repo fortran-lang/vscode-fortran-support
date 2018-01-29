43df88943fdb21ce2ed36bce31e07ce2f8ecde02
Miguel Carvajal <krvajal.miguelangel@gmail.com>
2018-01-29 08:38:58 -0300

    Add go to definition feature

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,46 @@
 // src/extension.ts
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-import FortranLintingProvider from './features/linter-provider';
-import FortranHoverProvider from './features/hover-provider';
-import { FortranCompletionProvider } from './features/completion-provider';
-import { FortranDocumentSymbolProvider } from './features/document-symbol-provider';
+import FortranLintingProvider from "./features/linter-provider";
+import FortranHoverProvider from "./features/hover-provider";
+import { FortranCompletionProvider } from "./features/completion-provider";
+import { FortranDocumentSymbolProvider } from "./features/document-symbol-provider";
+import FortranDefinitionProvider from "./features/definition-provider";
+import { TextDocument, DocumentHighlight } from "vscode";
+import { parseDoc } from "./lib/TagParser";
+const FORTRAN_MODE = "fortran90";
+export function activate(ctx: vscode.ExtensionContext) {
+  let hoverProvider = new FortranHoverProvider();
+  let completionProvider = new FortranCompletionProvider();
+  let symbolProvider = new FortranDocumentSymbolProvider();
+  if (vscode.workspace.getConfiguration("fortran").get("linterEnabled", true)) {
+    let linter = new FortranLintingProvider();
+    linter.activate(ctx.subscriptions);
+    vscode.languages.registerCodeActionsProvider("fortran90", linter);
+  }
 
-export function activate(context: vscode.ExtensionContext) {
+  vscode.languages.registerCompletionItemProvider(
+    "fortran90",
+    completionProvider
+  );
+  vscode.languages.registerHoverProvider("fortran90", hoverProvider);
+  vscode.languages.registerDocumentSymbolProvider("fortran90", symbolProvider);
 
-    let hoverProvider = new FortranHoverProvider();
-    let completionProvider = new FortranCompletionProvider();
-    let symbolProvider = new FortranDocumentSymbolProvider();
-
-    if (vscode.workspace.getConfiguration('fortran').get('linterEnabled', true)) {
-
-        let linter = new FortranLintingProvider();
-        linter.activate(context.subscriptions);
-        vscode.languages.registerCodeActionsProvider('fortran90', linter);
-    }
-
-    vscode.languages.registerCompletionItemProvider('fortran90', completionProvider);
-    vscode.languages.registerHoverProvider('fortran90', hoverProvider);
-    vscode.languages.registerDocumentSymbolProvider('fortran90', symbolProvider);
+  ctx.subscriptions.push(
+    vscode.languages.registerDefinitionProvider(
+      FORTRAN_MODE,
+      new FortranDefinitionProvider()
+    )
+  );
+  ctx.subscriptions.push(
+    vscode.workspace.onDidSaveTextDocument(tagsParserListener)
+  );
+  ctx.subscriptions.push(
+    vscode.workspace.onDidOpenTextDocument(tagsParserListener)
+  );
+}
+function tagsParserListener(textDocument: TextDocument) {
+  if (textDocument.languageId === FORTRAN_MODE) {
+    parseDoc(textDocument);
+  }
 }
