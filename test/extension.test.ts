8eabde7e8c285ea0c68b62986962dfa0da7d9e67
Miguel Carvajal <krvajal.miguelangel@gmail.com>
2018-04-15 19:05:08 +0200

    update travis setup

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -6,7 +6,6 @@
 // The module 'assert' provides assertion methods from node
 import * as assert from 'assert';
 import * as fs from 'fs';
-import { _loadDocString, intrinsics}  from '../src/lib/helper';
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
@@ -16,11 +15,7 @@ import * as myExtension from '../src/extension';
 // Defines a Mocha test suite to group tests of similar kind together
 suite('Extension Tests', () => {
 
-    // Defines a Mocha unit test
-    test('Something 1', () => {
-        assert.equal(-1, [1, 2, 3].indexOf(5));
-        assert.equal(-1, [1, 2, 3].indexOf(0));
-    });
+    
 
 });
 
