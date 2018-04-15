d0f85faaf1aeb9e5cdb47f2245c80b79fd29c078
Miguel Carvajal <krvajal.miguelangel@gmail.com>
2018-04-15 18:54:42 +0200

    Update tests

--- src/lib/tokenizer.ts
+++ src/lib/tokenizer.ts
@@ -1,60 +1,54 @@
-
-
-
 export enum TokenType {
-    FUNCTION,
-    SUBROUTINE,
-    NUMBER,
-    VARIABLE,
-    END,
-    DO,
-    IF,
-    LEFT_PARENTESIS,
-    RIGHT_PARENTESIS,
-    COMMA,
-    BINARY_OPERATOR
+  FUNCTION,
+  SUBROUTINE,
+  NUMBER,
+  VARIABLE,
+  END,
+  DO,
+  IF,
+  LEFT_PARENTESIS,
+  RIGHT_PARENTESIS,
+  COMMA,
+  BINARY_OPERATOR
 }
-
 
 export interface TokenInfo {
-
-    pattern: RegExp;
-    token: TokenType;
-
+  pattern: RegExp;
+  token: TokenType;
 }
+
 export interface Token {
-    token: TokenType;
-    sequence: string;
-
+  token: TokenType;
+  sequence: string;
 }
+
 export class Tokenizer {
-    
-    tokenInfos: TokenInfo[];
-    public tokens: Token[];
-    constructor(){
-        this.tokenInfos = [];
-        this.tokens = [];
-    }
-    public add(regex, token){
-        this.tokenInfos.push({pattern:regex, token: token});
-    }
-    public tokenize(expression:string){
-        this.tokens = [];
-        while(expression !== ''){
-            let match = false;
-            for(let i = 0; i < this.tokenInfos.length; i++){
-                let info = this.tokenInfos[i];
-                let result = info.pattern.exec(expression);
-                console.log(expression);
-                if(result && result.length > 0){
-                    match = true;
-                    this.tokens.push({token: info.token, sequence: result[0].trim()});
-                    expression = expression.replace(info.pattern,"");
-                    break;
-                }
-            }
+  tokenInfos: TokenInfo[];
+  public tokens: Token[];
+  constructor() {
+    this.tokenInfos = [];
+    this.tokens = [];
+  }
+
+  public add(regex, token) {
+    this.tokenInfos.push({ pattern: regex, token: token });
+  }
+  
+  public tokenize(expression: string) {
+    this.tokens = [];
+    while (expression !== "") {
+      let match = false;
+      for (let i = 0; i < this.tokenInfos.length; i++) {
+        let info = this.tokenInfos[i];
+        let result = info.pattern.exec(expression);
+
+        if (result && result.length > 0) {
+          match = true;
+          this.tokens.push({ token: info.token, sequence: result[0].trim() });
+          expression = expression.replace(info.pattern, "");
+          break;
         }
-
+      }
     }
-
+  }
 }
