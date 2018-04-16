6f5cf98dce0e8eaadb317e58f87a8a347eaa3624
Miguel Carvajal <krvajal.miguelangel@gmail.com>
2018-04-16 02:01:36 +0200

    make test pass

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -4,26 +4,30 @@
 //
 
 // The module 'assert' provides assertion methods from node
-import * as assert from 'assert';
-import * as fs from 'fs';
+import * as assert from "assert";
+import * as fs from "fs";
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
-import * as vscode from 'vscode';
-import * as myExtension from '../src/extension';
-import { FortranDocumentSymbolProvider } from '../src/features/document-symbol-provider';
+import * as vscode from "vscode";
+import * as myExtension from "../src/extension";
+import { FortranDocumentSymbolProvider } from "../src/features/document-symbol-provider";
 
 // Defines a Mocha test suite to group tests of similar kind together
-suite('Extension Tests', () => {
-
-    test("symbol provider works", async () => {
-      const editor = vscode.window.activeTextEditor;
-      await editor.show();
-      const symbolProvider = new FortranDocumentSymbolProvider()
-      const symbols = await symbolProvider.provideDocumentSymbols(editor.document, null);
+suite("Extension Tests", () => {
+  test(
+    "symbol provider works as expected",
+    async () => {
+      const filePath = "/test/resources/sample.f90";
+      const openPath = vscode.Uri.file(
+        `${vscode.workspace.rootPath}${filePath}`
+      );
+      const doc = await vscode.workspace.openTextDocument(openPath);
+      vscode.window.showTextDocument(doc);
+      const symbolProvider = new FortranDocumentSymbolProvider();
+      const symbols = await symbolProvider.provideDocumentSymbols(doc, null);
       assert.equal(symbols.length, 1);
-  });
-
+    },
+    20000
+  );
 });
-
-
