604403c5046650f3df7732c05b78accb3df442b9
Miguel Carvajal <krvajal.miguelangel@gmail.com>
2018-10-14 12:24:41 +0200

    Extract intrisinc list into its own file

--- src/features/completion-provider.ts
+++ src/features/completion-provider.ts
@@ -1,12 +1,8 @@
-import { CancellationToken, TextDocument, Position, Hover } from "vscode";
-import * as fs from "fs";
-import * as vscode from "vscode";
-import {
-  isPositionInString,
-  intrinsics,
-  FORTRAN_KEYWORDS
-} from "../lib/helper";
-import { getDeclaredFunctions } from "../lib/functions";
+import { CancellationToken, TextDocument, Position, Hover } from 'vscode'
+import * as fs from 'fs'
+import * as vscode from 'vscode'
+import { isPositionInString, intrinsics, FORTRAN_KEYWORDS } from '../lib/helper'
+import { getDeclaredFunctions } from '../lib/functions'
 
 export class FortranCompletionProvider
   implements vscode.CompletionItemProvider {
@@ -19,8 +15,8 @@ export class FortranCompletionProvider
       document,
       position,
       token,
-      vscode.workspace.getConfiguration("go")
-    );
+      vscode.workspace.getConfiguration('go')
+    )
   }
   public provideCompletionItemsInternal(
     document: vscode.TextDocument,
@@ -29,40 +25,40 @@ export class FortranCompletionProvider
     config: vscode.WorkspaceConfiguration
   ): Thenable<vscode.CompletionItem[]> {
     return new Promise<vscode.CompletionItem[]>((resolve, reject) => {
-      let filename = document.fileName;
-      let lineText = document.lineAt(position.line).text;
-      let lineTillCurrentPosition = lineText.substr(0, position.character);
+      let filename = document.fileName
+      let lineText = document.lineAt(position.line).text
+      let lineTillCurrentPosition = lineText.substr(0, position.character)
       // nothing to complete
       if (lineText.match(/^\s*\/\//)) {
-        return resolve([]);
+        return resolve([])
       }
 
-      let inString = isPositionInString(document, position);
+      let inString = isPositionInString(document, position)
       if (!inString && lineTillCurrentPosition.endsWith('"')) {
         // completing a string
-        return resolve([]);
+        return resolve([])
       }
 
       // get current word
-      let wordAtPosition = document.getWordRangeAtPosition(position);
-      let currentWord = "";
+      let wordAtPosition = document.getWordRangeAtPosition(position)
+      let currentWord = ''
       if (
         wordAtPosition &&
         wordAtPosition.start.character < position.character
       ) {
-        let word = document.getText(wordAtPosition);
+        let word = document.getText(wordAtPosition)
         currentWord = word.substr(
           0,
           position.character - wordAtPosition.start.character
-        );
+        )
       }
 
       if (currentWord.match(/^\d+$/)) {
         // starts with a number
-        return resolve([]);
+        return resolve([])
       }
 
-      let suggestions = [];
+      let suggestions = []
 
       if (currentWord.length > 0) {
         intrinsics.forEach(intrinsic => {
@@ -72,9 +68,9 @@ export class FortranCompletionProvider
                 intrinsic,
                 vscode.CompletionItemKind.Method
               )
-            );
+            )
           }
-        });
+        })
 
         // add keyword suggestions
         FORTRAN_KEYWORDS.forEach(keyword => {
@@ -84,11 +80,11 @@ export class FortranCompletionProvider
                 keyword.toLowerCase(),
                 vscode.CompletionItemKind.Keyword
               )
-            );
+            )
           }
-        });
+        })
       }
-      const functions = getDeclaredFunctions(document);
+      const functions = getDeclaredFunctions(document)
       // check for available functions
       functions.filter(fun => fun.name.startsWith(currentWord)).forEach(fun => {
         suggestions.push(
@@ -96,10 +92,10 @@ export class FortranCompletionProvider
             fun.name,
             vscode.CompletionItemKind.Function
           )
-        );
-      });
+        )
+      })
 
-      return resolve(suggestions);
-    });
+      return resolve(suggestions)
+    })
   }
 }
