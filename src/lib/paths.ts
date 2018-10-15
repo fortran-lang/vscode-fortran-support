ac442efa0f4f88589dcc9edaf8caa660c2ecdb84
Pedro Ricardo C. Souza <Pedro_Ricardo9@hotmail.com>
2018-10-15 13:54:35 -0300

    Correct errors found when using 'npm run lint'

--- src/lib/paths.ts
+++ src/lib/paths.ts
@@ -1,40 +1,40 @@
-import * as fs from 'fs'
-import * as path from 'path'
-import { toolBinNames } from './tools'
+import * as fs from 'fs';
+import * as path from 'path';
+import { toolBinNames } from './tools';
 
-let binPathCache: { [tool: string]: string } = {}
+let binPathCache: { [tool: string]: string } = {};
 
 export const envPath =
   process.env['PATH'] ||
-  (process.platform === 'win32' ? process.env['Path'] : null)
+  (process.platform === 'win32' ? process.env['Path'] : null);
 
 // checks in the PATH defined in the PATH env variables
 // for the specified tools and returns its complete path
 export default function getBinPath(tool: string): string | null {
-  if (binPathCache[tool]) return binPathCache[tool]
-  const binDirPaths = envPath.split(path.delimiter)
-  const binName = getBinName(tool)
+  if (binPathCache[tool]) return binPathCache[tool];
+  const binDirPaths = envPath.split(path.delimiter);
+  const binName = getBinName(tool);
   const possiblePaths = binDirPaths.map(binDirPath =>
     path.join(binDirPath, binName)
-  )
+  );
   for (let p of possiblePaths) {
     if (fileExists(p)) {
       // save in cache
-      binPathCache[tool] = p
-      return p
+      binPathCache[tool] = p;
+      return p;
     }
   }
-  return null
+  return null;
 }
 
 function getBinName(tool: string): string {
-  return toolBinNames[tool]
+  return toolBinNames[tool];
 }
 
 function fileExists(filePath: string): boolean {
   try {
-    return fs.statSync(filePath).isFile()
+    return fs.statSync(filePath).isFile();
   } catch (e) {
-    return false
+    return false;
   }
 }
