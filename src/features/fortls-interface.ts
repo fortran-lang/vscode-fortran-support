fcedc8bd66d233155d4360b0fdc91bf890173761
gnikit <giannis.nikiteas@gmail.com>
2022-04-20 14:34:18 +0100

    Updates `fortls` interface

--- src/features/fortls-interface.ts
+++ src/features/fortls-interface.ts
@@ -76,6 +76,45 @@ export class FortranLanguageServer {
 
     this.logger.logInfo('Initialising the Fortran Language Server');
 
+    const args: string[] = await this.fortlsArguments();
+    const executablePath = workspace.getConfiguration(EXTENSION_ID).get<string>('fortls.path');
+
+    // Detect language server version and verify selected options
+    this._fortlsVersion = this.getLSVersion(executablePath, args);
+    if (this._fortlsVersion) {
+      const serverOptions: ServerOptions = {
+        command: executablePath,
+        args: args,
+      };
+
+      // Options to control the language client
+      const clientOptions: LanguageClientOptions = {
+        // Register the server for all Fortran documents in workspace
+        // NOTE: remove the folder args and workspaceFolder to ge single file language servers
+        // will also have to change the checkLanguageServerActivation method
+        // we want fortran documents but not just in the workspace
+        // if in the workspace do provide
+        documentSelector: FortranDocumentSelector(folder),
+        workspaceFolder: folder,
+      };
+
+      // Create the language client, start the client and add it to the registry
+      const client = new LanguageClient(
+        'fortls',
+        'Fortran Language Server',
+        serverOptions,
+        clientOptions
+      );
+      client.start();
+      clients.set(folder.uri.toString(), client);
+    }
+  }
+
+  /**
+   * Sets `fortls` arguments based on VS Code settings
+   * @returns argument list
+   */
+  private async fortlsArguments() {
     // Get path for the language server
     const conf = workspace.getConfiguration(EXTENSION_ID);
     const executablePath = conf.get<string>('fortls.path');
@@ -94,27 +133,32 @@ export class FortranLanguageServer {
     // Enable all hover functionality. Does not make much sense to have one
     // but not the other two
     if (hover === 'fortls' || hover === 'Both') {
-      args.push('--variable_hover', '--hover_signature', '--use_signature_help');
+      args.push('--hover_signature', '--use_signature_help');
     }
     if (letterCase === 'lowercase') args.push('--lowercase_intrinsics');
 
     // FORTLS specific args with no overlap with the main extension
-    if (conf.get<boolean>('fortls.preserveKeywordOrder')) {
-      args.push('--preserve_keyword_order');
-    }
-    if (conf.get<boolean>('fortls.disableDiagnostics')) {
-      args.push('--disable_diagnostics');
-    }
-    if (conf.get<boolean>('fortls.incrementalSync')) {
-      args.push('--incremental_sync');
-    }
-    if (!conf.get<boolean>('fortls.symbolTypes')) {
-      args.push('--symbol_skip_mem');
+    if (conf.get<string>('fortls.configure')) {
+      args.push('-c', conf.get<string>('fortls.configure'));
     }
     if (conf.get<boolean>('fortls.notifyInit')) {
       args.push('--notify_init');
     }
-
+    if (conf.get<boolean>('fortls.incrementalSync')) {
+      args.push('--incremental_sync');
+    }
+    if (conf.get<boolean>('fortls.sortKeywords')) {
+      args.push('--sort_keywords');
+    }
+    if (conf.get<boolean>('fortls.disableAutoupdate')) {
+      args.push('--disable_autoupdate');
+    }
+    if (conf.get<boolean>('fortls.disableDiagnostics')) {
+      args.push('--disable_diagnostics');
+    }
+    if (!conf.get<boolean>('fortls.symbolTypes')) {
+      args.push('--symbol_skip_mem');
+    }
     if (maxLineLength > 0) {
       args.push(`--max_line_length=${maxLineLength}`);
     }
@@ -125,34 +169,7 @@ export class FortranLanguageServer {
       args.push(...fortlsExtraArgs);
     }
 
-    // Detect language server version and verify selected options
-    this._fortlsVersion = this.getLSVersion(executablePath, args);
-    if (this._fortlsVersion) {
-      // Check if there is a newer version
-      //  this.checkVersion(this._fortlsVersion);
-
-      const serverOptions: ServerOptions = {
-        command: executablePath,
-        args: args,
-      };
-
-      // Options to control the language client
-      const clientOptions: LanguageClientOptions = {
-        // Register the server for all Fortran documents in workspace
-        documentSelector: FortranDocumentSelector(folder),
-        workspaceFolder: folder,
-      };
-
-      // Create the language client, start the client and add it to the registry
-      const client = new LanguageClient(
-        'fortls',
-        'Fortran Language Server',
-        serverOptions,
-        clientOptions
-      );
-      client.start();
-      clients.set(folder.uri.toString(), client);
-    }
+    return args;
   }
 
   /**
