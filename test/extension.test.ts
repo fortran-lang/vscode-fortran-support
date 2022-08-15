8e64aaad360821e82974494835abb244c7e611d6
gnikit <giannis.nikiteas@gmail.com>
2022-08-15 10:53:35 +0100

    refactor: improves structure of integration test

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -4,7 +4,7 @@
 //
 
 // The module 'assert' provides assertion methods from node
-import * as assert from 'assert';
+import { strictEqual } from 'assert';
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
@@ -13,14 +13,18 @@ import * as path from 'path';
 import { FortranDocumentSymbolProvider } from '../src/features/document-symbol-provider';
 
 // Defines a Mocha test suite to group tests of similar kind together
-suite('Extension Tests', () => {
-  test('symbol provider works as expected', async () => {
-    const filePath = path.resolve(__dirname, '../../test/fortran/sample.f90');
-    const openPath = vscode.Uri.file(filePath);
-    const doc = await vscode.workspace.openTextDocument(openPath);
-    vscode.window.showTextDocument(doc);
+suite('Extension Integration Tests', async () => {
+  const filePath = path.resolve(__dirname, '../../test/fortran/sample.f90');
+  const openPath = vscode.Uri.file(filePath);
+  const doc = await vscode.workspace.openTextDocument(openPath);
+
+  suiteSetup(async () => {
+    await vscode.window.showTextDocument(doc);
+  });
+
+  test('Built-in symbol provider works as expected', async () => {
     const symbolProvider = new FortranDocumentSymbolProvider();
     const symbols = await symbolProvider.provideDocumentSymbols(doc, null);
-    assert.strictEqual(symbols.length, 1);
+    strictEqual(symbols.length, 1);
   });
 });
