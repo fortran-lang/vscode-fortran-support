1b8702c0c56ef40b3febea55e531ee1447693bac
gnikit <giannis.nikiteas@gmail.com>
2022-10-05 15:16:26 +0100

    test(linter): add basic linter activation tests
    Tests the activation, dispose, and linter commands

--- test/integration/runTest.ts
+++ test/integration/runTest.ts
@@ -1,6 +1,10 @@
 import * as path from 'path';
 
-import { runTests } from '@vscode/test-electron';
+import {
+  runTests,
+  downloadAndUnzipVSCode,
+  resolveCliArgsFromVSCodeExecutablePath,
+} from '@vscode/test-electron';
 import { spawnSync } from 'child_process';
 
 async function main() {
@@ -25,15 +29,22 @@ async function main() {
     // Passed to `--extensionDevelopmentPath`
     const extensionDevelopmentPath = path.resolve(__dirname, '../../../');
     const workspacePath = path.resolve(__dirname, '../../../test/fortran/');
+    // Install the C++ extension
+    const vscodeExecutablePath = await downloadAndUnzipVSCode('stable');
+    const [cli, ...args] = resolveCliArgsFromVSCodeExecutablePath(vscodeExecutablePath);
+    spawnSync(cli, [...args, '--install-extension', 'ms-vscode.cpptools'], {
+      encoding: 'utf-8',
+      stdio: 'inherit',
+    });
 
     // The path to the extension test runner script
     // Passed to --extensionTestsPath
     const extensionTestsPath = path.resolve(__dirname, './index');
 
-    const launchArgs = [workspacePath, '--disable-extensions'];
+    const launchArgs = [workspacePath];
     // Download VS Code, unzip it and run the integration test
     await runTests({
-      launchArgs,
+      launchArgs: launchArgs,
       extensionDevelopmentPath,
       extensionTestsPath,
     });
