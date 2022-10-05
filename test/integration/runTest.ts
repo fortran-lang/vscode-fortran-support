82acdc679bacc3529cbfdca06b90c8bf247d8e63
gnikit <giannis.nikiteas@gmail.com>
2022-10-05 15:28:57 +0100

    test(integration): disable the install of C++ ext

--- test/integration/runTest.ts
+++ test/integration/runTest.ts
@@ -30,18 +30,18 @@ async function main() {
     const extensionDevelopmentPath = path.resolve(__dirname, '../../../');
     const workspacePath = path.resolve(__dirname, '../../../test/fortran/');
     // Install the C++ extension
-    const vscodeExecutablePath = await downloadAndUnzipVSCode('stable');
-    const [cli, ...args] = resolveCliArgsFromVSCodeExecutablePath(vscodeExecutablePath);
-    spawnSync(cli, [...args, '--install-extension', 'ms-vscode.cpptools'], {
-      encoding: 'utf-8',
-      stdio: 'inherit',
-    });
+    // const vscodeExecutablePath = await downloadAndUnzipVSCode('stable');
+    // const [cli, ...args] = resolveCliArgsFromVSCodeExecutablePath(vscodeExecutablePath);
+    // spawnSync(cli, [...args, '--install-extension', 'ms-vscode.cpptools'], {
+    //   encoding: 'utf-8',
+    //   stdio: 'inherit',
+    // });
 
     // The path to the extension test runner script
     // Passed to --extensionTestsPath
     const extensionTestsPath = path.resolve(__dirname, './index');
 
-    const launchArgs = [workspacePath];
+    const launchArgs = [workspacePath, '--disable-extensions'];
     // Download VS Code, unzip it and run the integration test
     await runTests({
       launchArgs: launchArgs,
