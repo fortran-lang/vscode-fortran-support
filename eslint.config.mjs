a235a289c2acce78245f4cdc334a5724f5aca54f
gnikit <giannis.nikiteas@gmail.com>
2025-02-16 19:29:53 +0000

    build: migrate to ESlint v8+

---
+++ eslint.config.mjs
@@ -0,0 +1,62 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+import { fixupPluginRules, includeIgnoreFile } from '@eslint/compat';
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import _import from 'eslint-plugin-import';
+import jsdoc from 'eslint-plugin-jsdoc';
+import prettier from 'eslint-plugin-prettier';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+const gitignorePath = path.resolve(__dirname, '.gitignore');
+
+export default [
+  {
+    ignores: ['**/*.js', '**/test-resources'],
+  },
+  includeIgnoreFile(gitignorePath),
+  ...compat.extends('eslint:recommended', 'plugin:@typescript-eslint/recommended'),
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
+      jsdoc,
+      'import': fixupPluginRules(_import),
+      prettier,
+    },
+
+    languageOptions: {
+      parser: tsParser,
+    },
+
+    rules: {
+      'semi': [2, 'always'],
+      '@typescript-eslint/no-unused-vars': 0,
+      '@typescript-eslint/no-explicit-any': 0,
+      '@typescript-eslint/explicit-module-boundary-types': 0,
+      '@typescript-eslint/no-non-null-assertion': 0,
+
+      'import/order': [
+        'error',
+        {
+          'alphabetize': {
+            order: 'asc',
+          },
+
+          'newlines-between': 'always',
+          'groups': ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        },
+      ],
+    },
+  },
+];
