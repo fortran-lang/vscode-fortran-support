345cbfc1631e6dc221dc46d983eae26880abc717
gnikit <giannis.nikiteas@gmail.com>
2025-12-22 15:58:45 +0000

    test: assemble proper URIs for Windows

--- test/unitTest/tools.test.ts
+++ test/unitTest/tools.test.ts
@@ -5,6 +5,8 @@ import { Uri } from 'vscode';
 
 import { shellTask, spawnAsPromise, pathRelToAbs } from '../../src/util/tools';
 
+declare const __dirname: string;
+
 suite('Tools tests', () => {
   test('shellTask returns correct output', async () => {
     const name = 'pip: fortls';
@@ -34,8 +36,8 @@ suite('Tools tests', () => {
       const [stdout, stderr] = await spawnAsPromise('node', [
         path.resolve(__dirname, './exit-code-err.js'),
       ]);
-    } catch (error) {
-      const [stdout, stderr] = error;
+    } catch (error: any) {
+      const [stdout, stderr] = error as [string, string];
       assert.strictEqual(stdout, 'Hello World!');
       assert.strictEqual(stderr, 'Errors');
     }
@@ -54,19 +56,19 @@ suite('Tools tests', () => {
   });
 
   test('Resolve local paths: undefined', () => {
-    const root = Uri.parse('/home/user/project');
+    const root = Uri.file(path.resolve('/', 'home', 'user', 'project'));
     const absPath = pathRelToAbs('./sample.f90', root);
     assert.strictEqual(absPath, undefined);
   });
 
   test('Resolve local paths: absolute', () => {
-    const root = Uri.parse('/home/user/project');
+    const root = Uri.file(path.resolve('/', 'home', 'user', 'project'));
     const absPath = pathRelToAbs(path.resolve(process.cwd()), root);
     assert.strictEqual(absPath, path.resolve(process.cwd()));
   });
 
   test('Resolve local paths: workspace selection', () => {
-    const root = Uri.parse(path.resolve(__dirname, '../../../test/fortran'));
+    const root = Uri.file(path.resolve(__dirname, '../../../test/fortran'));
     const absPath = pathRelToAbs('./sample.f90', root);
     assert.strictEqual(absPath, path.resolve(__dirname, '../../../test/fortran/sample.f90'));
   });
